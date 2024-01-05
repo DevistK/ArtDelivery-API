3051cfae563c8e07d26e4b2e6968f05f7a33ab73
CatHero_V1 <tjehdgur1500@naver.com>
2024-01-06 03:21:49 +0900

    mod : 이미지 size 프로퍼티 수정

--- src/art/art.service.ts
+++ src/art/art.service.ts
@@ -2,30 +2,27 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ArtDto } from '../dto/art.dto';
 import OpenAI from 'openai';
 import { ResponseDto } from '../dto/response.dto';
+import { Size } from '../constant/enum';
 
 @Injectable()
 export class ArtService {
   private openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  public generate(data: ArtDto) {
+  async generate(data: ArtDto) {
     try {
-      // const image = await this.openai.images.generate({
-      //   model: 'dall-e-3',
-      //   prompt: data.prompt,
-      //   size: data.size,
-      //   quality: data.quality,
-      //   style: data.style,
-      //   n: 1,
-      // });
-      //
-      // return image.data[0].url;
-      return new ResponseDto(
-        200,
-        'https://via.placeholder.com/150',
-        'result image',
-      );
+      const image = await this.openai.images.generate({
+        model: 'dall-e-3',
+        prompt: data.prompt,
+        size: Size[data.size],
+        quality: data.quality,
+        style: data.style,
+        n: 1,
+      });
+
+      return new ResponseDto(200, image.data[0].url, 'result image');
     } catch (e) {
+      console.log(e);
       throw new InternalServerErrorException();
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['log', 'fatal', 'error', 'warn', 'debug'],
+  });
   await app.listen(8080);
   app.use(helmet({ contentSecurityPolicy: false }));
 }
